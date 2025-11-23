cd1f2b5f557b32c39658ce47b0f91ce84c2aeb27
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-23 02:34:00 +0100

    Add TokenResponseDto

---
+++ src/main/java/com/eyuup/security/payload/response/TokenResponseDto.java
@@ -0,0 +1,11 @@
+package com.eyuup.security.payload.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TokenResponseDto {
+    private String accessToken;
+    private String refreshToken;
+}
