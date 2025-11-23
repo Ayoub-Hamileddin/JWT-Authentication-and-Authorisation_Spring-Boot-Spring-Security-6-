d191025665f7fecb5c819e8172925b23165d348f
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-23 02:33:58 +0100

    Add model package

---
+++ src/main/java/com/eyuup/security/model/RefreshToken.java
@@ -0,0 +1,39 @@
+package com.eyuup.security.model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Date expireDate;
+
+    private Boolean revoked;
+
+    private LocalDateTime createdAt;
+
+}
---
+++ src/main/java/com/eyuup/security/model/Role.java
@@ -0,0 +1,8 @@
+package com.eyuup.security.model;
+
+public enum Role {
+  USER,
+  ADMIN
+
+  
+}
