b33d62169d5d4b2dab1835ba389a3e3e0b88603e
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-23 02:33:56 +0100

    Update LoginRequest payload

--- src/main/java/com/eyuup/security/payload/request/LoginRequest.java
+++ src/main/java/com/eyuup/security/payload/request/LoginRequest.java
@@ -1,14 +1,11 @@
 package com.eyuup.security.payload.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class LoginRequest {
 
  private String  email;
